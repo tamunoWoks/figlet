e50e45b39d4861266af2bf2639244dbed5d2945a
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-15 19:22:45 +0100

    Check if 2nd command line argument is in fonts and set font, else exit program with prompt

--- figlet.py
+++ figlet.py
@@ -12,3 +12,8 @@ elif len(sys.argv) == 3:
     if sys.argv[1] != '-f':
         if sys.srgv[1] != '--font':
             sys.exit('Invalid usage')
+
+    if sys.argv[2] in fonts:
+        figlet.setFont(font=sys.argv[2])
+    else:
+        sys.exit('Invalid usage')

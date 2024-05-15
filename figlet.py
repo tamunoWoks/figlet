152a927801a0decae5645e14bda7a5709e9897a7
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-15 19:18:32 +0100

    Check condition if command line argument is 1, set font, and check if the arguments are 3, if true check if 2nd argument is not -f or --font. if false exit program with prompt

--- figlet.py
+++ figlet.py
@@ -5,3 +5,10 @@ import sys
 figlet = Figlet()
 fonts = figlet.getFonts()
 
+if len(sys.argv) == 1:
+    style = random.choice(fonts)
+    figlet.setFont(font=style)
+elif len(sys.argv) == 3:
+    if sys.argv[1] != '-f':
+        if sys.srgv[1] != '--font':
+            sys.exit('Invalid usage')
